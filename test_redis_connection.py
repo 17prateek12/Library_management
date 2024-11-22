eef736ad0834a77c4bab26c5f73d914214c1b437
17prateek12 <88935432+17prateek12@users.noreply.github.com>
2024-11-22 18:25:43 +0530

    updates

--- test_redis_connection.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-try:
-    client = redis.StrictRedis(host='localhost', port=6380, db=0)
-    client.ping()
-    print("Successfully connected to Redis on port 6380!")
-except redis.ConnectionError:
-    print("Failed to connect to Redis on port 6380.")
